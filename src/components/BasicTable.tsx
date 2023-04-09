399720351b09466c7241c46e65280dd65300ca5d
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-04-09 09:07:28 -0700

    render columns and rows

---
+++ src/components/BasicTable.tsx
@@ -0,0 +1,61 @@
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS } from './columns'
+
+const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  // RECOMMENDS YOU TO MEMOIZE THE DATA AND COLUMNS TO PREVENNT UNNECESSARY RERENDERING
+  const tableInstance = useTable({
+    columns,
+    data
+  })
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance
+
+  return (
+    <div>
+      <table {...getTableProps}>
+        <thead>
+          {
+            headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {
+                  headerGroup.headers.map((column) => (
+                    <td {...column.getHeaderProps()}>{column.render('Header')}</td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </thead>
+        <tbody {...getTableBodyProps}>
+          {
+            rows.map((row) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {
+                    row.cells.map((cell) => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })
+                  }
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default BasicTable
